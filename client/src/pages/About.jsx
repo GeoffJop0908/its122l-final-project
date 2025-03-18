236fbdc536e8bad447cea82387667fc266b8de79
Charles Junjie A. Mempin <cjmempin18@gmail.com>
2025-03-18 17:15:49 +0800

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,3 +1,5 @@
+import { useNavigate, Link } from "react-router-dom";
+
 function About() {
   return (
     <>
@@ -9,28 +11,35 @@ function About() {
           <h1 className="text-5xl font-bold text-black">About The GCF</h1>
 
           <p className="text-lg text-black max-w-[60%] text-justify my-6 leading-relaxed">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+          Greenhills Christian Fellowship (GCF) was founded on February 14, 1978 by a missionary 
+          couple from America, Dave and PJ Yount. What started then as a Valentine’s Day event 
+          has now turned into a community of over 7,000 worshipers.
           </p>
 
           <p className="text-lg text-black max-w-[60%] text-justify my-6 leading-relaxed">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+          Throughout the years, GCF has grown to ‘Know Christ and Make Him Known’, with thousands supporting
+           missions-oriented efforts here and abroad for God’s glory. Today, GCF pursues 
+           a refocused mission of making disciples in the Philippines and beyond with a new vision of lives and 
+           communities transformed through Jesus Christ.
           </p>
 
+          <p className="text-lg text-black max-w-[60%] text-justify my-6 leading-relaxed">
+          GCF subscribes to gospel-centered expository preaching and Biblical eldership. We are a member of the 
+          Conservative Baptist Association of the Philippines (CBAP) and a partner of various Christian organizations 
+          in the Philippines and abroad.
+          </p>
+
+
+          <Link
+          to="https://www.gcf.org.ph/about"
+          className="font-medium hover:underline"
+          >
           <button className="bg-black text-white py-3 px-6 rounded-md mt-6">
+          
             Learn More
+          
           </button>
+          </Link>
         </div>
       </div>
     </>
