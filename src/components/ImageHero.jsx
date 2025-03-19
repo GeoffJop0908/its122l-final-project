34a163d10bbb18639ebc018c3ba4d0ac0a25e153
GeoffJop0908 <geoffjop0908@gmail.com>
2025-03-19 15:09:59 +0800

    controllable image hero

--- src/components/ImageHero.jsx
+++ src/components/ImageHero.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { cn } from '../lib/utils';
 
-export default function ImageHero({ right = false, className }) {
+export default function ImageHero({
+  right = false,
+  header,
+  image,
+  text,
+  button,
+  className,
+}) {
   return (
     <div
       className={cn('hero bg-stone-200 p-12 text-jungle-green-950', className)}
@@ -13,18 +20,15 @@ export default function ImageHero({ right = false, className }) {
           'lg:flex-row': !right,
         })}
       >
-        <img
-          src="gcf.jpg"
-          class="w-sm rounded-lg shadow-2xl"
-        />
+        <img src={image} class="w-sm rounded-lg shadow-2xl" />
         <div className="w-xl">
-          <h1 className="text-5xl font-bold">We're glad you're here!</h1>
-          <p className="py-6">
-            GCF San Fernando Pampanga started in 1998, but was formally launched on January 10, 1999 thru the collective efforts of missionary couple Pastor Love and Sister Racquel Tira; and pioneer members Brother Cesar Ocampo, and the couple Brother Mario and Sister Liz Dabu. In a formal inaugural service conducted at Days Hotel, attended by the leaders of the Conservative Baptist Association of the Philippines (CBAP), GCF-Ortigas, and its then senior pastor Dr. Luis Pantoja, Jr., GCF San Fernando was officially established to be a beacon of the gospel in Central Luzon with a primary goal: to “KNOW CHRIST AND MAKE HIM KNOWN.”
-          </p>
-          <button className="btn bg-jungle-green-500 border-none">
-            Worship with us!
-          </button>
+          <h1 className="text-5xl font-bold">{header}</h1>
+          <p className="py-6">{text}</p>
+          {button && (
+            <button className="btn bg-jungle-green-500 border-none">
+              {button}
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,22 @@ function Home() {
   return (
     <div className="-mt-[10vh]">
       <SliderHero />
-      <ImageHero right />
+      <ImageHero
+        right
+        text="GCF San Fernando Pampanga started in 1998, but was formally launched
+            on January 10, 1999 thru the collective efforts of missionary couple
+            Pastor Love and Sister Racquel Tira; and pioneer members Brother
+            Cesar Ocampo, and the couple Brother Mario and Sister Liz Dabu. In a
+            formal inaugural service conducted at Days Hotel, attended by the
+            leaders of the Conservative Baptist Association of the Philippines
+            (CBAP), GCF-Ortigas, and its then senior pastor Dr. Luis Pantoja,
+            Jr., GCF San Fernando was officially established to be a beacon of
+            the gospel in Central Luzon with a primary goal: to “KNOW CHRIST AND
+            MAKE HIM KNOWN."
+        image="/gcf.jpg"
+        header="We're glad you're here!"
+        button="Worship with us"
+      />
       <ImageHero className="bg-stone-300" />
     </div>
   );
