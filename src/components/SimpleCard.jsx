8f0fec75489202af4989b0504368d55270e989a9
GeoffJop0908 <geoffjop0908@gmail.com>
2025-03-19 14:58:36 +0800

    dashboard changes

--- src/components/SimpleCard.jsx
+++ src/components/SimpleCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { cn } from '../lib/utils';
 
-export default function SimpleCard({ children }) {
+export default function SimpleCard({ children, className }) {
   return (
     <div className="flex items-center justify-center h-[90vh] text-white">
-      <div className="bg-jungle-green-500 w-3/5 h-3/5 flex items-center justify-center rounded-xl text-4xl flex-col gap-5">
+      <div
+        className={cn(
+          'bg-jungle-green-500 w-3/5 h-3/5 flex items-center justify-center rounded-xl text-4xl flex-col gap-5',
+          className
+        )}
+      >
         {children}
       </div>
     </div>
--- src/pages/protected/Dashboard.jsx
+++ src/pages/protected/Dashboard.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import SimpleCard from '../../components/SimpleCard';
-// import useRefreshToken from '../hooks/useRefreshToken';
+import { motion } from 'framer-motion';
+import { IoHome } from 'react-icons/io5';
+import { MdEditCalendar } from 'react-icons/md';
+import { MdOutlineChat } from 'react-icons/md';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Dashboard() {
-  // const refresh = useRefreshToken();
+  return (
+    <SimpleCard className="p-10">
+      <h1>Welcome User</h1>
+      <div className="w-full h-full grid grid-cols-3 gap-5">
+        <Item text="Home" icon={<IoHome />} link="/" />
+        <Item
+          text="Appointment"
+          icon={<MdEditCalendar />}
+          link="/user/appointment"
+        />
+        <Item text="Feedback" icon={<MdOutlineChat />} link="/user/feedback" />
+      </div>
+    </SimpleCard>
+  );
+}
 
-  return <SimpleCard>Welcome User!</SimpleCard>;
+function Item({ text, icon, link }) {
+  const navigate = useNavigate();
+
+  return (
+    <motion.p
+      initial={{ y: 0 }}
+      whileHover={{ y: -10 }}
+      className="flex flex-col gap-3 items-center justify-center rounded-lg bg-jungle-green-600 border-jungle-green-950/10 border shadow-lg text-2xl cursor-pointer"
+      onClick={() => navigate(link)}
+    >
+      <span className="size-15 [&_svg]:size-full">{icon}</span>
+      {text}
+    </motion.p>
+  );
 }
